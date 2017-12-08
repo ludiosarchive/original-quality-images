fc9f71f2ea704c7bc4d139fe42dbd6d0509d3b86
Ivan Kozik <ivan@ludios.org>
2017-12-08 07:16:16 +0000

    Fix for URLs like https://pbs.twimg.com/media/DQcr39WVAAEOepz?format=jpg

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
-const twimg_re = new RegExp('^https://pbs\\.twimg\\.com/media/');
+// Match URLs like https://pbs.twimg.com/media/DQW9GvjX4AAkvDf.jpg
+// and             https://pbs.twimg.com/media/DQW9GvjX4AAkvDf.jpg:large
+// and             https://pbs.twimg.com/media/DQcr39WVAAEOepz?format=jpg
+const twimg_re = new RegExp('^https://pbs\\.twimg\\.com/media/[-_A-Za-z0-9]{15}');
 let debug = false;
 
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
 		debug && console.log("Got", details);
-		const url = details.url;
-		if (twimg_re.test(url)) {
-			const newUrl = url.replace(/:[a-z]{1,10}$/, '') + ':orig';
+		const url   = details.url;
+		const match = url.match(twimg_re);
+		if (match) {
+			const newUrl = match[0] + '.jpg:orig';
 			if (newUrl != url) {
 				debug && console.log("Redirecting to", newUrl);
 				return {redirectUrl: newUrl};
